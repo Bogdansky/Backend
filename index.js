dabf306182f9a4d6952206943cc7174d44956bd5
Bogdan <bogdya2@gmail.com>
2019-08-04 12:39:55 +0300

    add some functions

--- index.js
+++ index.js
@@ -5,12 +5,47 @@ const config = require('./config.json');
 const app = express();
 const context = require('./db/db')(Sequelize, config);
 
+let port = process.env.PORT || 5000;
+
 app.all('/api', (req, res) => {
 
 })
 
-app.get('/api/notes', getNotes)
+app.get('/api/notes', getNotes);
+app.post('/api/notes', createNote)
 
-function getNotes(req, res){
+async function getNotes(req, res){
     let id = req.query.id || 0;
+    let result = await context.note.findAll({
+        where: {
+            author_id: id
+        }
+    });
+    if (req.query.category){
+        result = result.filter((note) => note.categoryId == req.query.category);
+    }
+    res.json({result});
 }
+
+async function createNote(req,res){
+    let note = req.body;
+    if (!isValid(note)){
+        res.json({message: "Note is not valid"});
+        return;
+    }
+    note.categoryId = note.categoryId || 1;
+    let result = await context.note.create(note);
+    if (result){
+        res.json({id: result.id, message: "Note was created"})
+    } else {
+        res.json({message: "Note wasn't created"})
+    }
+}
+
+function isValid(note){
+    return note.title && note.text && note.author_id;
+}
+
+app.listen(port, () => {
+    console.log("server is started")
+})
